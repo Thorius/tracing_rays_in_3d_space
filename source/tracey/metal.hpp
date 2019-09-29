d59552de386395ff485b81af09eaec846329c65a
Stefan Ivanov <ivanov.st94@gmail.com>
2019-09-29 04:58:02 +0300

    Add metal materials

--- source/tracey/metal.hpp
+++ source/tracey/metal.hpp
@@ -1,20 +1,25 @@
-#pragma once 
+#pragma once
 
 #include "material.hpp"
-#include "vector3.hpp"
 #include "ray.hpp"
+#include "vector3.hpp"
 
 class Metal : public Material {
-public:
-    Metal(Vector3 const& attenuation) : albedo(attenuation) {}
-    bool scatter(Ray const& ray, HitRecord const& record, Vector3 & attenuation, Ray & scattered) const override;
+  public:
+    Metal(Vector3 const &attenuation) : albedo(attenuation) {}
+    bool scatter(Ray const &ray, HitRecord const &record, Vector3 &attenuation,
+                 Ray &scattered) const override;
 
-private:
+  private:
     Vector3 albedo;
 };
 
-bool Metal::scatter(Ray const& ray, HitRecord const& record, Vector3 & attenuation, Ray & scattered) const
-{
-    // TODO: Write scatter function
-    return false;
+bool Metal::scatter(Ray const &ray, HitRecord const &record,
+                    Vector3 &attenuation, Ray &scattered) const {
+    Vector3 reflected = reflect(unit_vector(ray.direction()), record.normal);
+    scattered = Ray(record.p, reflected);
+    attenuation = albedo;
+    // There is a scattered ray only if the external ray has hit the surface on
+    // the outside
+    return dot(scattered.direction(), record.normal) > 0.0f;
 }
