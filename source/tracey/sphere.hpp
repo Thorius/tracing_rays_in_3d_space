00ad67f7ff1d8ae863d7a57f8d1904507ab16646
Stefan Ivanov <ivanov.st94@gmail.com>
2019-09-29 03:17:14 +0300

    Complete sphere intersection

--- source/tracey/sphere.hpp
+++ source/tracey/sphere.hpp
@@ -13,6 +13,26 @@ class Sphere: public Hittable  {
 };
 
 bool Sphere::hit(const Ray& r, float t_min, float t_max, HitRecord& rec) const {
-    // TODO
+    Vector3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = dot(oc, r.direction());
+    float c = dot(oc, oc) - radius*radius;
+    float discriminant = b*b - a*c;
+    if (discriminant > 0) {
+        float temp = (-b - std::sqrt(discriminant))/a;
+        if (temp < t_max && temp > t_min) {
+            rec.t = temp;
+            rec.p = r.point_at_parameter(rec.t);
+            rec.normal = (rec.p - center) / radius;
+            return true;
+        }
+        temp = (-b + std::sqrt(discriminant)) / a;
+        if (temp < t_max && temp > t_min) {
+            rec.t = temp;
+            rec.p = r.point_at_parameter(rec.t);
+            rec.normal = (rec.p - center) / radius;
+            return true;
+        }
+    }
     return false;
 }
